5e93c59bb1261d43b50815a8b49c9a783222a639
sini <tlsgml818@puzzledata.com>
2025-11-24 15:18:05 +0900

    kakao login

--- user-service/src/main/kotlin/com/covercloud/user/domain/User.kt
+++ user-service/src/main/kotlin/com/covercloud/user/domain/User.kt
@@ -2,6 +2,8 @@ package com.covercloud.user.domain
 
 import com.covercloud.shared.BaseEntity
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -11,10 +13,19 @@ import jakarta.persistence.Table
 @Table(name = "user")
 class User(
     val socialId: String,
-    val provider: String,
+    @Enumerated(EnumType.STRING)
+    val provider: Provider? = null,
     val nickname: String,
     val profileImage: String,
 ) : BaseEntity() {
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long? = null
+}
+
+enum class Provider(val value: String){
+    KAKAO("kakao");
+
+    companion object {
+        fun from(value: String): Provider = Provider.values().first { it.value == value }
+    }
 }
